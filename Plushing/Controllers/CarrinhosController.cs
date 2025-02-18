da03909794d73709c02772cc5ba8942130fe99f3
May <senaidev.may@gmail.com>
2025-02-18 11:55:31 -0300

    criação e adição de endpoint no carrinhoscontroller

---
+++ Plushing/Controllers/CarrinhosController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plushing.Data;
+using Plushing.Models;
+
+namespace Plushing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrinhosController : ControllerBase
+    {
+        private readonly PlushingContext _context;
+
+        public CarrinhosController(PlushingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Carrinhos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Carrinho>>> GetCarrinhos()
+        {
+            return await _context.Carrinhos.ToListAsync();
+        }
+
+        // GET: api/Carrinhos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Carrinho>> GetCarrinho(Guid id)
+        {
+            var carrinho = await _context.Carrinhos.FindAsync(id);
+
+            if (carrinho == null)
+            {
+                return NotFound();
+            }
+
+            return carrinho;
+        }
+
+        // PUT: api/Carrinhos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarrinho(Guid id, Carrinho carrinho)
+        {
+            if (id != carrinho.CarrinhoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carrinho).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarrinhoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Carrinhos
+        [HttpPost]
+        public async Task<ActionResult<Carrinho>> PostCarrinho(Carrinho carrinho)
+        {
+            _context.Carrinhos.Add(carrinho);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCarrinho", new { id = carrinho.CarrinhoId }, carrinho);
+        }
+
+        // DELETE: api/Carrinhos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCarrinho(Guid id)
+        {
+            var carrinho = await _context.Carrinhos.FindAsync(id);
+            if (carrinho == null)
+            {
+                return NotFound();
+            }
+
+            _context.Carrinhos.Remove(carrinho);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // GET: api/Carrinhos/ByDate
+        [HttpGet("ByDate")]
+        public async Task<ActionResult<IEnumerable<Carrinho>>> GetCarrinhosByDate(DateTime startDate, DateTime endDate)
+        {
+            var carrinhos = await _context.Carrinhos
+                .Where(c => c.DataCriacao >= startDate && c.DataCriacao <= endDate)
+                .ToListAsync();
+
+            if (carrinhos == null || !carrinhos.Any())
+            {
+                return NotFound();
+            }
+
+            return carrinhos;
+        }
+
+        private bool CarrinhoExists(Guid id)
+        {
+            return _context.Carrinhos.Any(e => e.CarrinhoId == id);
+        }
+    }
+}
