ab7c51edae1fff97f8183830744849b2fb7cd977
Aluna.Lorena <lorennasenaia@gmail.com>
2025-02-18 11:43:00 -0300

    Adição controlers Acessorio e tamanhos

---
+++ Plushing/Controllers/AcessoriosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plushing.Data;
+using Plushing.Models;
+
+namespace Plushing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcessoriosController : ControllerBase
+    {
+        private readonly PlushingContext _context;
+
+        public AcessoriosController(PlushingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Acessorios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Acessorio>>> GetAcessorios()
+        {
+            return await _context.Acessorios.ToListAsync();
+        }
+
+        // GET: api/Acessorios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Acessorio>> GetAcessorio(Guid id)
+        {
+            var acessorio = await _context.Acessorios.FindAsync(id);
+
+            if (acessorio == null)
+            {
+                return NotFound();
+            }
+
+            return acessorio;
+        }
+
+        // PUT: api/Acessorios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAcessorio(Guid id, Acessorio acessorio)
+        {
+            if (id != acessorio.AcessorioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(acessorio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AcessorioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Acessorios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Acessorio>> PostAcessorio(Acessorio acessorio)
+        {
+            _context.Acessorios.Add(acessorio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAcessorio", new { id = acessorio.AcessorioId }, acessorio);
+        }
+
+        // DELETE: api/Acessorios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAcessorio(Guid id)
+        {
+            var acessorio = await _context.Acessorios.FindAsync(id);
+            if (acessorio == null)
+            {
+                return NotFound();
+            }
+
+            _context.Acessorios.Remove(acessorio);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AcessorioExists(Guid id)
+        {
+            return _context.Acessorios.Any(e => e.AcessorioId == id);
+        }
+    }
+}
