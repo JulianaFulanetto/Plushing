b73995c14cfd0abb3745d820c65318c0e034b3c7
Juliana <juliana.fulanetto@aluno.senai.br>
2025-02-18 08:42:28 -0300

    Conclusão das models tamanho

--- Plushing/Models/Personalizacao.cs
+++ Plushing/Models/Personalizacao.cs
@@ -7,7 +7,8 @@ namespace Plushing.Models
         public Guid PersonalizacaoId { get; set; }
         public Guid CorId { get; set; }
         public Cor? Cor { get; set; }
-        public string TextoPersonalizado { get; set; }
+        public string Presenteado { get; set; }
+        public string NomePelucia { get; set; }
 
     }
 }
---
+++ Plushing/Models/Tamanho.cs
@@ -0,0 +1,8 @@
+﻿namespace Plushing.Models
+{
+    public class Tamanho
+    {
+        public int TamanhoId { get; set; }
+        public string Descricao { get; set; }
+    }
+}
