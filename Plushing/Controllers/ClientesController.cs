eb33ecc9f7fd18f4a796d5ebdee038ba823420ac
Juliana <juliana.fulanetto@aluno.senai.br>
2025-02-18 11:48:29 -0300

    Criação do ControllerClientes e adição dos EndPoints

---
+++ Plushing/Controllers/ClientesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plushing.Data;
+using Plushing.Models;
+
+namespace Plushing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly PlushingContext _context;
+
+        public ClientesController(PlushingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> GetCliente(Guid id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+
+        // PUT: api/Clientes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(Guid id, Cliente cliente)
+        {
+            if (id != cliente.ClienteId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clientes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCliente", new { id = cliente.ClienteId }, cliente);
+        }
+
+        // DELETE: api/Clientes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCliente(Guid id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClienteExists(Guid id)
+        {
+            return _context.Clientes.Any(e => e.ClienteId == id);
+        }
+
+        // GET: api/Clientes/cpf/{cpf}
+        [HttpGet("cpf/{cpf}")]
+        public async Task<ActionResult<Cliente>> GetClienteByCpf(string cpf)
+        {
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(c => c.Cpf == cpf);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+
+        // GET: api/Clientes/nome/{nome}
+        [HttpGet("nome/{nome}")]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientesByName(string nome)
+        {
+            var clientes = await _context.Clientes.Where(c => c.Nome.Contains(nome)).ToListAsync();
+
+            if (clientes == null || !clientes.Any())
+            {
+                return NotFound();
+            }
+
+            return clientes;
+        }
+
+        // PATCH: api/Clientes/{id}/telefone
+        [HttpPatch("{id}/telefone")]
+        public async Task<IActionResult> UpdateClienteTelefone(Guid id, [FromBody] string telefone)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            cliente.Telefone = telefone;
+            _context.Entry(cliente).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
